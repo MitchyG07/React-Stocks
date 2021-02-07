d8233b9c5a5053aed9b08ccb9561f753bb86a0f2
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-02-06 16:38:40 -0800

    complete

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,19 +1,20 @@
 import React from 'react'
 
-const Stock = () => (
+const Stock = (props) => (
   <div>
 
-    <div className="card">
+
+    <div onClick={() => props.clickEvent(props.deets)} className="card">
       <div className="card-body">
         <h5 className="card-title">{
-            //Company Name
+            props.deets.name
           }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
+        <p className="card-text">
+          {props.deets.ticker}: {props.deets.price}
+          </p>
       </div>
     </div>
-
+    
 
   </div>
 );
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,92 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    stocks: [],
+    myPortfolio: [],
+    allStocks: [],
+    checked: false
+  } 
+
+  componentDidMount() {
+    fetch('http://localhost:3000/stocks')
+    .then(res => res.json())
+    .then(data => this.setState({
+      stocks: data,
+      allStocks: data
+    }))
+  }
+
+  filterStocks = (event) => {
+    let filtered = this.state.allStocks.filter(stock => stock.type === event.target.value)
+    this.setState({
+      stocks: filtered
+    })
+  }
+
+  addToPortfolio = (stock) => {
+      fetch(`http://localhost:3000/stocks/${stock.id}`)
+      .then(res => res.json())
+      .then(data => this.setState({
+        myPortfolio: [...this.state.myPortfolio, data]
+      }), console.log(this.state.myPortfolio))
+  }
+
+  sellStock = (stock) => {
+    let newPortfolio = this.state.myPortfolio.filter(sold => sold != stock)
+    this.setState({
+      myPortfolio: newPortfolio
+    })
+  }
+
+  sortByName = () => {
+    let updateCheck = !this.state.checked
+    if (updateCheck === true) {
+      let sorted = this.state.stocks.sort((a, b) => a.name.localeCompare(b.name))
+      this.setState({
+        stocks: sorted,
+        checked: updateCheck
+      })
+    } else {
+      let sorted = this.state.allStocks
+      this.setState({
+        stocks: sorted,
+        checked: null
+      })
+    }
+  }
+
+  sortByPrice = () => {
+    let updateCheck = !this.state.checked
+    if (updateCheck === true) {
+      let sorted = this.state.stocks.sort((a, b) => (b.price - a.price))
+      this.setState({
+        stocks: sorted,
+        checked: updateCheck
+      })
+    } else {
+      let sorted = this.state.allStocks
+      this.setState({
+        stocks: sorted,
+        checked: null
+      })
+    }
+  }
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar sortByName={this.sortByName} sortByPrice={this.sortByPrice} filterStocks={this.filterStocks} checked={this.state.checked}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer deets={this.state.stocks} clickEvent={this.addToPortfolio} />
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer portfolio={this.state.myPortfolio} clickEvent={this.sellStock}/>
 
             </div>
           </div>
